081d41a932991fc9a2c61eb7a95a9df7c73deab9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 19:55:24 +0300

    Setup assemblies

---
+++ Assets/UGF.Models.Runtime.Tests/TestModelUtility.cs
@@ -0,0 +1,6 @@
+namespace UGF.Models.Runtime.Tests
+{
+    public class TestModelUtility
+    {
+    }
+}
---
+++ Packages/UGF.Models/Runtime/ModelUtility.cs
@@ -0,0 +1,6 @@
+namespace UGF.Models.Runtime
+{
+    public class ModelUtility
+    {
+    }
+}
