0d1a048eb3d55cc10537b0b4b5b36e5e07a1800b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-30 15:15:20 +0300

    Update TestModels.cs

--- Assets/UGF.Models.Runtime.Tests/TestModels.cs
+++ Assets/UGF.Models.Runtime.Tests/TestModels.cs
@@ -2,6 +2,7 @@
 using UGF.Application.Runtime;
 using UGF.Models.Runtime.Domain;
 using UGF.Module.Controllers.Runtime;
+using UGF.RuntimeTools.Runtime.Contexts;
 using UnityEngine;
 
 namespace UGF.Models.Runtime.Tests
@@ -18,6 +19,28 @@ namespace UGF.Models.Runtime.Tests
             var controller = application.GetController<IDomainModelOperatorController>();
 
             Assert.NotNull(controller);
+
+            application.Uninitialize();
+        }
+
+        [Test]
+        public void Execute()
+        {
+            IApplication application = CreateApplication();
+            var asset = Resources.Load<DomainModelAsset>("DomainModel");
+            var domainModel = asset.Build<IDomainModel>();
+            var model = domainModel.Get<TestModel>("c7ec7dee8c761684ebb0e85d3f6189c3");
+            var context = new Context { application };
+
+            application.Initialize();
+
+            var controller = application.GetController<IModelController>("b148c8ecb988147488302960be6c74ca");
+
+            Assert.AreEqual(0, model.Value);
+
+            controller.Execute(domainModel, context);
+
+            Assert.AreEqual(1, model.Value);
 
             application.Uninitialize();
         }
