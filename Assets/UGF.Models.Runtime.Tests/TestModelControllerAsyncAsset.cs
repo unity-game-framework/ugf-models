cea54a5d99ee9314253c26e9b24974121283dd2c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-30 15:21:38 +0300

    Add execute async tests

---
+++ Assets/UGF.Models.Runtime.Tests/TestModelControllerAsyncAsset.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using UGF.Application.Runtime;
+using UGF.Module.Controllers.Runtime;
+using UGF.RuntimeTools.Runtime.Contexts;
+using UnityEngine;
+
+namespace UGF.Models.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestModelControllerAsyncAsset")]
+    public class TestModelControllerAsyncAsset : ModelControllerAsset
+    {
+        protected override IController OnBuild(IApplication arguments)
+        {
+            return new TestModelControllerAsync(arguments);
+        }
+    }
+
+    public class TestModelControllerAsync : ModelControllerAsync<TestModel>
+    {
+        public TestModelControllerAsync(IApplication application) : base(application)
+        {
+        }
+
+        protected override Task OnExecuteAsync(TestModel model, IContext context)
+        {
+            model.Value++;
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Assets/UGF.Models.Runtime.Tests/TestModels.cs
+++ Assets/UGF.Models.Runtime.Tests/TestModels.cs
@@ -1,9 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using System.Threading.Tasks;
+using NUnit.Framework;
 using UGF.Application.Runtime;
 using UGF.Models.Runtime.Domain;
 using UGF.Module.Controllers.Runtime;
 using UGF.RuntimeTools.Runtime.Contexts;
+using UGF.RuntimeTools.Runtime.Tasks;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace UGF.Models.Runtime.Tests
 {
@@ -39,6 +43,30 @@ namespace UGF.Models.Runtime.Tests
             Assert.AreEqual(0, model.Value);
 
             controller.Execute(domainModel, context);
+
+            Assert.AreEqual(1, model.Value);
+
+            application.Uninitialize();
+        }
+
+        [UnityTest]
+        public IEnumerator ExecuteAsync()
+        {
+            IApplication application = CreateApplication();
+            var asset = Resources.Load<DomainModelAsset>("DomainModel");
+            var domainModel = asset.Build<IDomainModel>();
+            var model = domainModel.Get<TestModel>("c7ec7dee8c761684ebb0e85d3f6189c3");
+            var context = new Context { application };
+
+            application.Initialize();
+
+            var controller = application.GetController<IModelControllerAsync>("f5a327fd871186045b44471425082a61");
+
+            Assert.AreEqual(0, model.Value);
+
+            Task task = controller.ExecuteAsync(domainModel, context);
+
+            yield return task.WaitCoroutine();
 
             Assert.AreEqual(1, model.Value);
 
