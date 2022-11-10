f91b4eecc42a9a5fcf9886b46e67d9df1802fdaf
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-10 19:31:58 +0100

    Add DomainModelCollectionModel

---
+++ Packages/UGF.Models/Runtime/Domain/DomainModelCollectionModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UGF.Models.Runtime.Domain
+{
+    public class DomainModelCollectionModel : IModel
+    {
+        public List<Guid> ModelIds { get; set; } = new List<Guid>();
+    }
+}
