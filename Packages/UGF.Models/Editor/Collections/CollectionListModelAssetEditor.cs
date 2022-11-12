a7d128afcc23106a45c9ff497d59c3601238c7ee
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-12 16:26:53 +0100

    Add editors

---
+++ Packages/UGF.Models/Editor/Collections/CollectionListModelAssetEditor.cs
@@ -0,0 +1,38 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Models.Runtime.Collections;
+using UnityEditor;
+
+namespace UGF.Models.Editor.Collections
+{
+    [CustomEditor(typeof(CollectionListModelAsset), true)]
+    internal class CollectionListModelAssetEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_listItems;
+
+        private void OnEnable()
+        {
+            m_listItems = new ReorderableListDrawer(serializedObject.FindProperty("m_items"))
+            {
+                DisplayAsSingleLine = true
+            };
+
+            m_listItems.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listItems.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listItems.DrawGUILayout();
+            }
+        }
+    }
+}
