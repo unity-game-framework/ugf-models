39ecf535f2a3b4a16cab87331ef4eb8d1acdd82f
Vorobey <vorobyovvorobyov@gmail.com>
2023-01-14 19:38:04 +0100

    Add RelationCollectionModel

---
+++ Packages/UGF.Models/Runtime/Relations/RelationCollectionModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UGF.Models.Runtime.Relations
+{
+    public class RelationCollectionModel<TModel> : Dictionary<Guid, TModel> where TModel : class, IModel
+    {
+    }
+}
