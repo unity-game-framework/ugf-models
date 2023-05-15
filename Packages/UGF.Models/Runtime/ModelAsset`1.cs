f7c97452b39bd69589f5f098ed1e02c65a21dc83
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-15 15:16:16 +0200

    Add ModelAsset

---
+++ Packages/UGF.Models/Runtime/ModelAsset`1.cs
@@ -0,0 +1,12 @@
+﻿namespace UGF.Models.Runtime
+{
+    public abstract class ModelAsset<TModel> : ModelAsset where TModel : IModel
+    {
+        protected override IModel OnBuild()
+        {
+            return OnBuildTyped();
+        }
+
+        protected abstract TModel OnBuildTyped();
+    }
+}
