3c9aecf3c9d52d1a3ebacc17ec461080b0c89b08
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-15 15:25:53 +0200

    Add ModelCollectionListAsset

---
+++ Packages/UGF.Models/Editor/ModelCollectionListAssetEditor.cs
@@ -0,0 +1,45 @@
+﻿using UGF.EditorTools.Editor.Assets;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Models.Runtime;
+using UnityEditor;
+
+namespace UGF.Models.Editor
+{
+    [CustomEditor(typeof(ModelCollectionListAsset), true)]
+    internal class ModelCollectionListAssetEditor : UnityEditor.Editor
+    {
+        private AssetIdReferenceListDrawer m_listModels;
+        private ReorderableListSelectionDrawerByPath m_listModelsSelection;
+
+        private void OnEnable()
+        {
+            m_listModels = new AssetIdReferenceListDrawer(serializedObject.FindProperty("m_models"));
+
+            m_listModelsSelection = new ReorderableListSelectionDrawerByPath(m_listModels, "m_asset")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listModels.Enable();
+            m_listModelsSelection.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listModels.Disable();
+            m_listModelsSelection.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listModels.DrawGUILayout();
+                m_listModelsSelection.DrawGUILayout();
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Models/Runtime/ModelCollectionAsset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.Models.Runtime
+{
+    public abstract class ModelCollectionAsset : ScriptableObject
+    {
+        public void GetModels(IDictionary<Guid, IModel> models)
+        {
+            if (models == null) throw new ArgumentNullException(nameof(models));
+
+            OnGetModels(models);
+        }
+
+        protected abstract void OnGetModels(IDictionary<Guid, IModel> models);
+    }
+}
