fd1d4c26af7ce500f62a5ad1136bdc3799ebd781
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-15 15:30:01 +0200

    Change DomainModelAsset

--- Packages/UGF.Models/Editor/Domain/DomainModelAssetEditor.cs
+++ Packages/UGF.Models/Editor/Domain/DomainModelAssetEditor.cs
@@ -13,6 +13,8 @@ namespace UGF.Models.Editor.Domain
         private ReorderableListSelectionDrawerByPath m_listModelsSelection;
         private AssetIdReferenceListDrawer m_listCollections;
         private ReorderableListSelectionDrawerByPath m_listCollectionsSelection;
+        private ReorderableListDrawer m_listModelCollections;
+        private ReorderableListSelectionDrawerByElement m_listModelCollectionsSelection;
 
         private void OnEnable()
         {
@@ -36,10 +38,19 @@ namespace UGF.Models.Editor.Domain
                 Drawer = { DisplayTitlebar = true }
             };
 
+            m_listModelCollections = new ReorderableListDrawer(serializedObject.FindProperty("m_modelCollections"));
+
+            m_listModelCollectionsSelection = new ReorderableListSelectionDrawerByElement(m_listModelCollections)
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
             m_listModels.Enable();
             m_listModelsSelection.Enable();
             m_listCollections.Enable();
             m_listCollectionsSelection.Enable();
+            m_listModelCollections.Enable();
+            m_listModelCollectionsSelection.Enable();
         }
 
         private void OnDisable()
@@ -48,6 +59,8 @@ namespace UGF.Models.Editor.Domain
             m_listModelsSelection.Disable();
             m_listCollections.Disable();
             m_listCollectionsSelection.Disable();
+            m_listModelCollections.Disable();
+            m_listModelCollectionsSelection.Disable();
         }
 
         public override void OnInspectorGUI()
@@ -58,9 +71,11 @@ namespace UGF.Models.Editor.Domain
 
                 m_listModels.DrawGUILayout();
                 m_listCollections.DrawGUILayout();
+                m_listModelCollections.DrawGUILayout();
 
                 m_listModelsSelection.DrawGUILayout();
                 m_listCollectionsSelection.DrawGUILayout();
+                m_listModelCollectionsSelection.DrawGUILayout();
             }
         }
     }
