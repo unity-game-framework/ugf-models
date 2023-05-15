d40079df20cff290128e04741b268c897aba1cd3
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-15 15:06:57 +0200

    Add ICollectionModel

--- Packages/UGF.Models/Runtime/Collections/CollectionDictionaryModel`1.cs
+++ Packages/UGF.Models/Runtime/Collections/CollectionDictionaryModel`1.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 namespace UGF.Models.Runtime.Collections
 {
-    public class CollectionDictionaryModel<TModel> : IModel where TModel : IModel
+    public class CollectionDictionaryModel<TModel> : ICollectionModel where TModel : IModel
     {
+        public int Count { get { return Models.Count; } }
         public Dictionary<Guid, TModel> Models { get; set; } = new Dictionary<Guid, TModel>();
+
+        public void Clear()
+        {
+            Models.Clear();
+        }
     }
 }
--- Packages/UGF.Models/Runtime/Collections/CollectionListModel`1.cs
+++ Packages/UGF.Models/Runtime/Collections/CollectionListModel`1.cs
@@ -2,8 +2,14 @@
 
 namespace UGF.Models.Runtime.Collections
 {
-    public class CollectionListModel<TModel> : IModel where TModel : IModel
+    public class CollectionListModel<TModel> : ICollectionModel where TModel : IModel
     {
+        public int Count { get { return Models.Count; } }
         public List<TModel> Models { get; set; } = new List<TModel>();
+
+        public void Clear()
+        {
+            Models.Clear();
+        }
     }
 }
