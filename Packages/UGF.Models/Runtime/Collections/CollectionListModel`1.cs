98237470f0da1855f9708d5a786d8f0fe8b93dab
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-22 14:05:33 +0200

    Update CollectionListModel`1.cs

--- Packages/UGF.Models/Runtime/Collections/CollectionListModel`1.cs
+++ Packages/UGF.Models/Runtime/Collections/CollectionListModel`1.cs
@@ -5,7 +5,12 @@ namespace UGF.Models.Runtime.Collections
     public class CollectionListModel<TModel> : ICollectionModel where TModel : IModel
     {
         public int Count { get { return Models.Count; } }
-        public List<TModel> Models { get; set; } = new List<TModel>();
+        public List<TModel> Models { get; set; }
+
+        public CollectionListModel(int capacity = 4)
+        {
+            Models = new List<TModel>(capacity);
+        }
 
         public void Clear()
         {
