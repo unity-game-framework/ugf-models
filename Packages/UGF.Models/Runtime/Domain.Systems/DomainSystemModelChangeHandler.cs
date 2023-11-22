751e1cafb830d1f57b8935fdae09977d000fd141
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-22 20:35:55 +0300

    Remove system models

--- Packages/UGF.Models/Runtime/Domain.Systems/DomainSystemModelChangeHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using UGF.RuntimeTools.Runtime.Contexts;
-
-namespace UGF.Models.Runtime.Domain.Systems
-{
-    public delegate void DomainSystemModelChangeHandler(IDomainSystemModel systemModel, int index, Guid id, IContext context);
-}
